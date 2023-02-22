0b173ff6307238fde8d52ea0953cf37901169c47
QuinnMatthews <Quinn.Matthews@lrs.com>
2023-02-22 10:33:21 -0600

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,3 @@
+wget "https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi.tar.xz?rev=7bd049b7a3034e64885fa1a71c12f91d&hash=732D909FA8F68C0E1D0D17D08E057619arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi.tar.xz\?rev\=7bd049b7a3034e64885fa1a71c12f91d" -O "arm-gnu-toolchain.tar.xz"
+tar -xf "arm-gnu-toolchain.tar.xz"
+rm "arm-gnu-toolchain.tar.xz"
